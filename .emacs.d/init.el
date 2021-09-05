b79003e0dc880e2ce6fb7d9f65b6579577977772
Genki Sato <13384803+gksato@users.noreply.github.com>
2021-09-06 01:23:53 +0900

    highlight-indent-guides introduced

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -63,6 +63,15 @@
 (leaf flycheck
   :ensure t)
 
+(leaf highlight-indent-guides
+  :ensure t
+  :blackout t
+  :hook (((prog-mode-hook yaml-mode-hook) . highlight-indent-guides-mode))
+  :custom (
+           (highlight-indent-guides-method . 'character)
+           (highlight-indent-guides-auto-enabled . t)
+           (highlight-indent-guides-responsive . t)
+           (highlight-indent-guides-character . ?|)))
 
 (leaf which-key
   :ensure t
